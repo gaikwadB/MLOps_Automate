2bc6ae10c8f78b38cf4a854eb25382aed90aa2a4
gaikwadB <gaikwadbhaiyyasaheb@gmail.com>
2020-05-30 10:38:47 +0530

    First commited

---
+++ MsgThroughMail.py
@@ -0,0 +1,12 @@
+import smtplib
+rcvr = "gaikwadsiddhant66@gmail.com"
+sender = "gaikwadbhaiyyasaheb@gmail.com"
+msg = "Hey Developer, your model has greater than 95% accuracy and is ready to use for predictions!!" 
+pswd = "Bhaiyya@358" #password for sender's mail address
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.ehlo()
+server.starttls()
+server.login(sender,pswd)
+print("Access Granted")
+server.sendmail(sender,rcvr,msg)
+print("Mail sent!!")
