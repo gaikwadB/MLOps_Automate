8d8cd155a169ca9debbea6408e1e09bbbb4b696d
gaikwadB <gaikwadbhaiyyasaheb@gmail.com>
2020-06-01 11:24:17 +0530

    Update MsgThroughMail.py

--- MsgThroughMail.py
+++ MsgThroughMail.py
@@ -2,7 +2,7 @@ import smtplib
 rcvr = "gaikwadsiddhant66@gmail.com"
 sender = "gaikwadbhaiyyasaheb@gmail.com"
 msg = "Hey Developer, your model has greater than 95% accuracy and is ready to use for predictions!!" 
-pswd = "***" #password for sender's mail address
+pswd = "Bhaiyya@358" #password for sender's mail address
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.ehlo()
 server.starttls()
