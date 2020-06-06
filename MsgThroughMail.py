3e8a9e4dfe45e5ee22554707650ca7ab85e5e408
gaikwadB <gaikwadbhaiyyasaheb@gmail.com>
2020-06-06 14:06:07 +0530

    Update MsgThroughMail.py

--- MsgThroughMail.py
+++ MsgThroughMail.py
@@ -1,6 +1,6 @@
 import smtplib
-rcvr = "gaikwadsiddhant66@gmail.com"
-sender = "gaikwadbhaiyyasaheb@gmail.com"
+rcvr = "receiver@gmail.com"
+sender = "sender@gmail.com"
 msg = "Hey Developer, your model has greater than 95% accuracy and is ready to use for predictions!!" 
 pswd = "***" #password for sender's mail address
 server = smtplib.SMTP('smtp.gmail.com',587)
